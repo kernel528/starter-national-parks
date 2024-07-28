82d54b22b726de8af443de55f179e0fe53d75bdc
Joe Sanders <kernel528@gmail.com>
2024-07-27 19:04:52 -0500

    Added section 5.1.7 related content.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const submitHandler = (event) => {
 
   const form = document.querySelector("#park-form");
   const formData = new FormData(form);
+
   // Keep track of if any errors are found
   let hasErrors = false;
 
@@ -18,52 +19,34 @@ const submitHandler = (event) => {
 
   // if there are no errors
   if (!hasErrors) {
-    //create a new element
-    const parkSection = document.createElement("section");
+    // Create an empty object
+    const park = {
+      name: formData.get("name"),
+      location: formData.get("location"),
+      description: formData.get("description"),
+      established: formData.get("established"),
+      area: formData.get("area"),
+      rating: formData.get("rating"),
+    };
 
-    // add the park class
-    parkSection.classList.add("park-display");
+    parks.push(park);
 
-    // construct the HTML for this element
-    const content = `
-    <h2>${formData.get("name")}</h2>
-    <div class="location-display">${formData.get("location")}</div>
-    <div class="description-display">${formData.get("description")}</div>
-    <button class="rate-button" title="Add to Favourites">&#9734;</button>
-    <div class="stats">
-      <div class="established-display stat">
-        <h3>Established</h3>
-        <div class="value">${formData.get("established")}</div>
-      </div>
-      <div class="area-display stat">
-        <h3>Area</h3>
-        <div class="value">${formData.get("area")}</div>
-      </div>
-      <div class="rating-display stat">
-        <h3>Rating</h3>
-        <div class="value">${formData.get("rating")}</div>
-      </div>
-    </div>
-    `;
-
-    // set the innerHTML
-    parkSection.innerHTML = content;
-
-    //append to the main element
-    document.querySelector("main").appendChild(parkSection);
+    render();
   }
 };
 
 // function to handler favorite button clicks
 const favoriteButtonClickHandler = (event) => {
-  const park = event.target.parentNode;
-  park.style.backgroundColor = "#c8e6c9";
+  if (event.target && event.target.nodeName === "BUTTON") {
+    const park = event.target.parentNode;
+    park.style.backgroundColor = "#c8e6c9";
+  }
 };
 
 // function for sorting by name
 const sortByName = (parkA, parkB) => {
-  const parkAName = parkA.querySelector("h2").innerText;
-  const parkBName = parkB.querySelector("h2").innerText;
+  const parkAName = parkA.name;
+  const parkBName = parkB.name;
   if (parkAName < parkBName) {
     return -1;
   } else if (parkAName > parkBName) {
@@ -75,64 +58,78 @@ const sortByName = (parkA, parkB) => {
 
 // function for sorting by rating
 const sortByRating = (parkA, parkB) => {
-  const parkARating = parseFloat(
-    parkA.querySelector(".rating-display > .value").innerText
-  );
-  const parkBRating = parseFloat(
-    parkB.querySelector(".rating-display > .value").innerText
-  );
-  return parkBRating - parkARating;
+  const parkARating = parseFloat(parkA.rating);
+  const parkBRating = parseFloat(parkB.rating);
+  if (parkARating < parkBRating) {
+    return 1;
+  } else if (parkARating > parkBRating) {
+    return -1;
+  } else {
+    return 0;
+  }
 };
 
 // function for handling the nameSorter click
 const nameSorterClickHandler = (event) => {
   event.preventDefault();
 
-  // 1.  get the main element
-  const main = document.querySelector("main");
+  parks.sort(sortByName);
 
-  // 2. get the list of parks
-  const parksList = main.querySelectorAll(".park-display");
-
-  // 3. empty the main
-  main.innerHTML = "";
-
-  // 4. create an array
-  const parksArray = Array.from(parksList);
-
-  // 5. sort the array
-  parksArray.sort(sortByName);
-
-  // 6. Insert each park into the DOM
-  parksArray.forEach((park) => {
-    main.appendChild(park);
-  });
+  render();
 };
 
 // function to handle the ratingSorter click
 const ratingSorterClickHandler = (event) => {
   event.preventDefault();
 
-  // 1.  get the main element
+  parks.sort(sortByRating);
+
+  render();
+};
+
+const renderOnePark = (park) => {
+  // Get the individual properties of the park
+  const { name, location, description, established, area, rating } = park;
+
+  const content = `
+      <section class="park-display">
+        <h2>${name}</h2>
+        <div class="location-display">${location}</div>
+        <div class="description-display">${description}</div>
+        <button class="rate-button" title="Add to Favourites">&#9734;</button>
+        <div class="stats">
+          <div class="established-display stat">
+            <h3>Established</h3>
+            <div class="value">${established}</div>
+          </div>
+          <div class="area-display stat">
+            <h3>Area</h3>
+            <div class="value">${area}</div>
+          </div>
+          <div class="rating-display stat">
+            <h3>Rating</h3>
+            <div class="value">${rating}</div>
+          </div>
+        </div>
+      </section>
+  `;
+  return content;
+};
+
+const render = () => {
+  // Get the parent element
   const main = document.querySelector("main");
 
-  // 2. get the list of parks
-  const parksList = main.querySelectorAll(".park-display");
-
-  // 3. empty the main
+  // Empty the parent element
   main.innerHTML = "";
 
-  // 4. create an array
-  const parksArray = Array.from(parksList);
+  // Get the parks HTML
+  const content = parks.map(renderOnePark).join("");
 
-  // 5. sort the array
-  parksArray.sort(sortByRating);
-
-  // 6. Insert each park into the DOM
-  parksArray.forEach((park) => {
-    main.appendChild(park);
-  });
+  // Set the `innerHTML` of parent element
+  main.innerHTML = content;
 };
+
 
 // the point where all the code starts
 const main = () => {
@@ -152,15 +149,24 @@ const main = () => {
   const allBtns = document.querySelectorAll(".rate-button");
 
   // iterate the list of buttons and add an event handler to each
-  allBtns.forEach((btn) => {
-    btn.addEventListener("click", favoriteButtonClickHandler);
-  });
+  // allBtns.forEach((btn) => {
+  //   btn.addEventListener("click", favoriteButtonClickHandler);
+  // });
+
+  // Select all the buttons for all the parks
+  const main = document.querySelector("main");
+
+  // Add event handler to the main
+  main.addEventListener("click", favoriteButtonClickHandler);
 
   // get the form element
   const form = document.querySelector("#park-form");
 
   // attach the submit handler
   form.addEventListener("submit", submitHandler);
+
+  // call the render function
+  render();
 };
 
 // Add event listener for DOMContentLoaded
---
+++ voyaguers.js
@@ -0,0 +1,10 @@
+
+{
+    name: "Voyageurs National Park",
+        location: "Minnesota, United States",
+    description:
+    "Voyageurs National Park is in northern Minnesota, near the Canadian border. It covers a vast area and is known for its forests, waterways and huge, island-dotted Rainy, Kabetogama and Namakan lakes. The Ellsworth Rock Gardens, created by artist Jack Ellsworth, are a series of abstract sculptures on a terraced outcrop. The remote Kettle Falls area has a dam and a red-roofed hotel, both from the early 20th century.",
+        established: "April 8, 1975",
+    area: "218,200 acres (883 km2)",
+    rating: 4.9,
+},
